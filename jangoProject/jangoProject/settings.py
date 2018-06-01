9670b6ba427cec33c3ffa11c47f9e7bd018f7ffd
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-06-01 12:32:36 +1000

    changed db to sqlite3

--- jangoProject/jangoProject/settings.py
+++ jangoProject/jangoProject/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'jangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'JangoDB',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
 }
 
 
