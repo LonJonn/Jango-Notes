4284903d7da0ee51d43deea6b538ba5456fb3880
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-06-09 11:23:26 +1000

    added validation for due dates set date created to auto generate

--- jangoMain/notesApp/models.py
+++ jangoMain/notesApp/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 
@@ -10,13 +11,13 @@ class Note(models.Model):
         ('green' ,'Green'),
         ('yellow' ,'Yellow'),
     )
-    owner = models.IntegerField()
+    owner = models.PositiveSmallIntegerField()
     title = models.CharField(max_length=20)
     description = models.TextField()
     colour = models.CharField(max_length=20, choices=colourChoices)
     subject = models.CharField(max_length=20)
-    dateCreated = models.DateTimeField('date created')
-    dateDue = models.DateTimeField('date due')
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateDue = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -24,3 +25,7 @@ class Note(models.Model):
     def isNew(self):
         return self.dateCreated >= timezone.now() - datetime.timedelta(days=2)
 
+    def clean(self):
+        if self.dateDue is not None:
+            if self.dateDue < timezone.now():
+                raise ValidationError({"dateDue":"The due date cannot be in the past!"})
