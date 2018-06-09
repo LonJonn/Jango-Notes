7967d72ebb4d71605c5bb1f59bc4a23fda1f8105
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-06-09 11:29:41 +1000

    made some choices optional

--- jangoMain/notesApp/models.py
+++ jangoMain/notesApp/models.py
@@ -12,10 +12,10 @@ class Note(models.Model):
         ('yellow' ,'Yellow'),
     )
     owner = models.PositiveSmallIntegerField()
-    title = models.CharField(max_length=20)
-    description = models.TextField()
-    colour = models.CharField(max_length=20, choices=colourChoices)
-    subject = models.CharField(max_length=20)
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True, null=True)
+    colour = models.CharField(blank=True, null=True, max_length=30, choices=colourChoices)
+    subject = models.CharField(blank=True, null=True, max_length=30)
     dateCreated = models.DateTimeField(auto_now_add=True)
     dateDue = models.DateTimeField(blank=True, null=True)
 
