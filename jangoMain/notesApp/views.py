fa313a5e42129c42f6797a862cf10fbf60ff2259
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-06-10 02:36:42 +1000

    added base.html for templating added static files for images and styling

--- jangoMain/notesApp/views.py
+++ jangoMain/notesApp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Note
+from django.contrib.auth.models import User
 
 
 def index(request):
-    return render(request, 'notesApp/index.html', {})
+    numNotes = Note.objects.all().count()
+    numUsers = User.objects.all().count()
+    return render(request, 'notesApp/index.html', {'numUsers': numUsers, 'numNotes': numNotes})
 
 def notes(request):
     notes = Note.objects.all()
-    return render(request, 'notesApp/notes.html', {'notes': notes})
+    numNotes = notes.count()
+    return render(request, 'notesApp/notes.html', {'notes': notes, 'numNotes': numNotes})
 
 def noteInfo(request):
     return render(request, 'notesApp/noteInfo.html')
