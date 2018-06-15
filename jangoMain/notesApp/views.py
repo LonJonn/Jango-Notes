0bab530c9ff181c7a7ddaa11a6b32eacc22c8873
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-06-15 13:53:46 +1000

    added basic users system also added password reset

--- jangoMain/notesApp/views.py
+++ jangoMain/notesApp/views.py
@@ -7,13 +7,16 @@ from django.contrib.auth.models import User
 def index(request):
     numNotes = Note.objects.all().count()
     numUsers = User.objects.all().count()
+
     return render(request, 'notesApp/index.html', {'numUsers': numUsers, 'numNotes': numNotes})
 
 def notes(request):
     notes = Note.objects.all()
     numNotes = notes.count()
+
     return render(request, 'notesApp/notes.html', {'notes': notes, 'numNotes': numNotes})
 
 def editNote(request, pk):
     note = get_object_or_404(Note, pk=pk)
+    
     return render(request, 'notesApp/editNote.html', {'note': note})
