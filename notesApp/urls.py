9f330781c7a2e8495d7535a416e7d0580151ef97
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-06-25 23:11:18 +1000

    changed home

--- notesApp/urls.py
+++ notesApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [     #define urls used in the note app accross the site
-    path('', views.Index, name='index'),
+    path('home', views.Index, name='index'),
     path('notes', views.Notes, name='notes'),
     path('note/<int:pk>', views.EditNote, name='editNote'),
     path('note/<int:pk>/delete', views.DeleteNote, name='deleteNote'),
