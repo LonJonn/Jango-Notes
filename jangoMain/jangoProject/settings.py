fbb4f676c0159b7b47ac714b9c72b466b6fbc904
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-06-25 22:30:18 +1000

    and settings

--- jangoMain/jangoProject/settings.py
+++ jangoMain/jangoProject/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
